1d39fbcefcdc6dae32affda3aaf336ae06123783
aqilamuzafa917 <147041734+aqilamuzafa917@users.noreply.github.com>
2025-06-11 17:49:44 +0700

    fix chatbot fix api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  baseURL: "https://thankful-urgently-silkworm.ngrok-free.app",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "ngrok-skip-browser-warning": "69420",
     // "Content-Type": "application/json",
